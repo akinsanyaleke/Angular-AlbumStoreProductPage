5786ba3d3b041badead1aa30bbdaa1fc7678439c
akinsanyaleke <akinsanyaleke@users.noreply.github.com>
2018-04-08 16:17:42 -0400

    creating a service to get album data

---
+++ src/app/product.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx'
+
+@Injectable()
+export class ProductService {
+  private _albumUrl : string = "../assets/album.json";
+  constructor(private _http: Http) { }
+  private extractAlbum(response: Response){
+    let body = response.json();
+    return body || {};
+  }
+  private handleError(error:any){
+    console.error('Observable error:', error);
+    return Observable.throw(error.statusText);
+  }
+  getAlbum(id : number) : Observable<any> {
+    return this._http.get(this._albumUrl).map(this.extractAlbum).catch(this.handleError);
+  }
+}
